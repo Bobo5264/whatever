01ac30104ade278b32b563d96d5af87d8212b867
Bobo5264 <evanbowen52@gmail.com>
2025-04-05 17:26:01 -0500

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

---
+++ images.g.ts
@@ -0,0 +1,52 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Hello":return img`
+.....cccccccccccccc.....
+...cbd111111111111dbc...
+..cd1111111111111111dc..
+.cd111111111111111111dc.
+.b11111111111111111111b.
+cd11111111111111111111dc
+c1111121121112221111111c
+c1111121121111211111111c
+c1111121121111211111111c
+c1111122221111211111111c
+c1111121121111211111111c
+c1111121121111211111111c
+c1111121121112221111111c
+c1111111111111111111111c
+c1111111111111111111111c
+c1111111111111111111111c
+cd11111111111111111111dc
+cb11111111111111111111bc
+ccd111111111111111111dc.
+.ccd1111111111111111dcc.
+..c111111111111111dbcc..
+.b11dcccccccccccccccc...
+cddcccccccccccccccc.....
+ccccc...................
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
--- main.ts
+++ main.ts
@@ -1,6 +1,78 @@
+namespace SpriteKind {
+    export const NPC = SpriteKind.create()
+    export const Misc = SpriteKind.create()
+}
+sprites.onCreated(SpriteKind.Enemy, function (sprite) {
+    tiles.placeOnRandomTile(sprite, sprites.castle.tilePath5)
+    sprite.follow(playerOne, 25)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.builtin.forestTiles10, function (sprite, location) {
+    shopkeep1 = sprites.create(img`
+        . . . . f f f f . . . . . 
+        . . f f f f f f f f . . . 
+        . f f f f f f c f f f . . 
+        f f f f f f c c f f f c . 
+        f f f c f f f f f f f c . 
+        c c c f f f e e f f c c . 
+        f f f f f e e f f c c f . 
+        f f f b f e e f b f f f . 
+        . f 4 1 f 4 4 f 1 4 f . . 
+        . f e 4 4 4 4 4 4 e f . . 
+        . f f f e e e e f f f . . 
+        f e f b 7 7 7 7 b f e f . 
+        e 4 f 7 7 7 7 7 7 f 4 e . 
+        e e f 6 6 6 6 6 6 f e e . 
+        . . . f f f f f f . . . . 
+        . . . f f . . f f . . . . 
+        `, SpriteKind.NPC)
+    shopkeep1.setPosition(10, 21)
+    game.showLongText("General Store", DialogLayout.Top)
+    furniture = sprites.create(img`
+        ..cccc.........
+        .cdddbc........
+        cbdddbccccccc..
+        cbdddbcbdddddc.
+        cbdddbcdddddbbc
+        cbdddbcbbbbbbbc
+        cbdddbccccccbbc
+        cbdddbcddddbccc
+        cbdddbcdddddbc.
+        cbdddbcdddddbc.
+        cbdddbcdddddbc.
+        cbdddbcdddddbc.
+        cbdddbcdddddbc.
+        cbdddbcdddddbc.
+        cbdddbcdddddbc.
+        cbddbbcdddddbc.
+        cbbbbbcccccccc.
+        cbbbbbcbdddddc.
+        cbbbbbcdddddbbc
+        cbbbbbcbbbbbbbc
+        cbbbbbcbbbbbbbc
+        cbbbbbcbbbbbbbc
+        .cccccccccccccc
+        .cbbc.....cbbc.
+        `, SpriteKind.Misc)
+    furniture.setPosition(10, 51)
+    controller.moveSprite(playerOne, 0, 0)
+    tiles.setCurrentTilemap(tilemap`level2`)
+    pause(500)
+    tiles.placeOnTile(playerOne, tiles.getTileLocation(4, 7))
+    controller.moveSprite(playerOne, 100, 100)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.builtin.field1, function (sprite, location) {
+    tiles.setCurrentTilemap(tilemap`level1`)
+    tiles.placeOnTile(playerOne, tiles.getTileLocation(5, 13))
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    game.gameOver(false)
+})
+let furniture: Sprite = null
+let shopkeep1: Sprite = null
+let playerOne: Sprite = null
 tiles.setCurrentTilemap(tilemap`level1`)
 scene.setBackgroundColor(7)
-let mySprite = sprites.create(img`
+playerOne = sprites.create(img`
     . . . . . . f f f f . . . . . . 
     . . . . f f f 2 2 f f f . . . . 
     . . . f f f 2 2 2 2 f f f . . . 
@@ -18,5 +90,5 @@ let mySprite = sprites.create(img`
     . . . . . f f f f f f . . . . . 
     . . . . . f f . . f f . . . . . 
     `, SpriteKind.Player)
-scene.cameraFollowSprite(mySprite)
-controller.moveSprite(mySprite, 100, 100)
+scene.cameraFollowSprite(playerOne)
+controller.moveSprite(playerOne, 100, 100)
